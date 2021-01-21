04616cc1175307ae9cbd5c15e3674322061b4d99
Daniel Williams <dancart2001@gmail.com>
2021-01-21 13:58:48 +0000

    progress bar

--- kmusic/player/static/player/js/dist/player.dev.js
+++ kmusic/player/static/player/js/dist/player.dev.js
@@ -2,14 +2,42 @@
 
 document.addEventListener('DOMContentLoaded', function () {
   controls_section = document.querySelector('#controls-section');
+  updateProgressBar(); //pause button
+
   time_left = parseFloat(controls_section.dataset.timeleft);
   pause_btn = document.querySelector('#pause_btn');
-  console.log(time_left);
 
-  if (time_left > 0) {
-    setTimeout(refresh, time_left);
+  if (pause_btn != null) {
+    //progress bar
+    progress_interval = setInterval(updateProgressBar, 100);
   }
+
+  refresh_timeout = setTimeout(refresh, time_left);
+
+  pause_btn.onclick = function () {
+    clearTimeout(refresh_timeout);
+    clearInterval(progress_interval);
+  };
+
+  console.log(time_left); //play button
+
+  play_btn = document.querySelector('#play_btn');
+  refresh_timeout = setTimeout(refresh, time_left);
+
+  play_btn.onclick = function () {
+    refresh_timeout = setTimeout(refresh, time_left);
+    progress_interval = setInterval(updateProgressBar, 100);
+  };
 });
+
+function updateProgressBar() {
+  progress_div = document.querySelector('#song-progress');
+  progress = parseFloat(progress_div.dataset.songprogress);
+  percentage = 100 * (progress / parseFloat(progress_div.dataset.songduration)); //console.log(percentage+"%");
+
+  progress_div.style.width = percentage + "%";
+  progress_div.setAttribute("data-songprogress", progress + 100);
+}
 
 function refresh() {
   pause_btn = document.querySelector('#pause_btn');
