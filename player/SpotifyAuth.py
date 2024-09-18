2349dc611dcb6ab514f6b50aaf48a6f5791979da
ish10k <111194744+ish10k@users.noreply.github.com>
2024-09-18 07:49:36 +0100

    refactor getAccessToken function

--- player/SpotifyAuth.py
+++ player/SpotifyAuth.py
@@ -3,31 +3,32 @@ import json
 
 import requests
 from decouple import config
+import base64
 
 
 class SpotifyAuth():
     access_token = None
     refresh_token = None
-    expirey_time=None
+    expiry_time=None
 
     code = None
     auth = None
 
-    def __init__(self, code=None, access_token=None, refresh_token=None, expirey_time=None):
+    def __init__(self, code=None, access_token=None, refresh_token=None, expiry_time=None):
         #if allows us to have two different constructors
         if code!=None:
             #print("code constructor")
             self.code = code
             self.access_token=None
             self.refresh_token=None
-            self.expirey_time=None
+            self.expiry_time=None
             self.auth=None
             self.getAccessToken()
-        elif access_token!=None and refresh_token!=None and expirey_time!=None:
+        elif access_token!=None and refresh_token!=None and expiry_time!=None:
             #print("token constructor")
             self.access_token=access_token
             self.refresh_token=refresh_token
-            self.expirey_time=self.stringToDateTime(expirey_time)
+            self.expiry_time=self.stringToDateTime(expiry_time)
 
     def stringToDateTime(self, string):
         # time = "2020-06-05 16:30:34.392897"
@@ -38,63 +39,61 @@ class SpotifyAuth():
         return "Bearer " + accessToken
 
     def getAccessToken(self):
-        # if expirey time not set, we assume the token has already expired
         now = datetime.datetime.now()
-        #print("self.expirey_time", self.expirey_time)
-        if self.expirey_time==None:
-            #print("token not yet set")
-            data = {
-                "client_id": config('SPOTIFY_CID'),
-                #should be hidden
-                "client_secret": config('SPOTIFY_CS'),
-                "grant_type":"authorization_code",
-                "code":self.code,
-                "redirect_uri":config('REDIRECT_URI'),
-            }
-            tokenRes = requests.post("https://accounts.spotify.com/api/token", data = data)
-            response = json.loads(tokenRes.text)
-            #print("response ",response)
-            #print("exires in: ",response["expires_in"])
-            self.expirey_time=self.calculateExpireyTime(response["expires_in"])
-            self.refresh_token = response["refresh_token"]
-            self.access_token = response["access_token"]
+
+        # early circuit breaker
+        if self.expiry_time is not None and self.expiry_time > now:
+            print("token still valid, expires", self.expiry_time)
             return self.access_token
-        elif self.expirey_time<=now:
-            #print("token expired, time to get a new one")
-            data = {
-                "grant_type":"refresh_token",
-                "refresh_token":self.refresh_token,
-                "client_id": config('SPOTIFY_CID'),
-                #should be hidden
-                "client_secret": config('SPOTIFY_CS'),
-                "redirect_uri": config('REDIRECT_URI'),
-            }
-            tokenRes = requests.post("https://accounts.spotify.com/api/token", data = data)
-            response = json.loads(tokenRes.text)
-            self.expirey_time=self.calculateExpireyTime(response["expires_in"])
-            self.access_token = response["access_token"]
-            return self.access_token
+
+        headers = {
+            "Authorization": "Basic " + base64.b64encode(f"{config('SPOTIFY_CID')}:{config('SPOTIFY_CS')}".encode()).decode("utf-8"),
+            "content-type": "application/x-www-form-urlencoded"
+        }
+        payload = {
+            "code":self.code,
+            "redirect_uri":config('REDIRECT_URI'),
+        }
+
+        # No expiry time means no token, so we need to fetch a new one.
+        # If we have an expiry time and reached this point, it means the token must be expired
+        # and we need to get a new one!
+        if self.expiry_time is None:
+            print("token not yet set")
+            payload["grant_type"] = "authorization_code"
         else:
-           #print("token still valid")
-            #print(self.expirey_time)
-            return self.access_token
+            print("token expired, time to get a new one")
+            payload["grant_type"] = "refresh_token"
+
+        response = requests.post("https://accounts.spotify.com/api/token", data=payload, headers=headers)
+
+        if response.status_code >= 400:
+            raise Exception("Could not obtain new token, response", response.text()) 
+
+        resJson = response.json()
+        print("obtained new token, response", resJson)
+        self.expiry_time=self.calculateExpiryTime(resJson["expires_in"])
+        self.refresh_token = resJson["refresh_token"]
+        self.access_token = resJson["access_token"]
+            
+        return self.access_token
 
     def getRefreshToken(self):
         return self.refresh_token
         
-    def getExpireyTime(self):
-        return self.expirey_time
+    def getexpiryTime(self):
+        return self.expiry_time
 
     def toJSON(self):
         d = {
             "access_token" : self.access_token,
             "refresh_token" : self.refresh_token,
-            "expirey_time": str(self.expirey_time),
+            "expiry_time": str(self.expiry_time),
         }
         return json.dumps(d)
-        #expirey_time is a datetime object so must be serialized...
+        #expiry_time is a datetime object so must be serialized...
 
-    def calculateExpireyTime(self, secondsUntilExpirey):
+    def calculateExpiryTime(self, secondsUntilexpiry):
         now = datetime.datetime.now()
-        expireyTime = now + datetime.timedelta(seconds=secondsUntilExpirey)
-        return expireyTime
+        expiryTime = now + datetime.timedelta(seconds=secondsUntilexpiry)
+        return expiryTime
